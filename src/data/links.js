d66980bdd5eb3b97dce9480abe4dafca82b412af
Khaseem <mrwallacelk@gmail.com>
2026-07-03 08:43:36 -0700

    dropbox location update

---
+++ src/data/links.js
@@ -0,0 +1,5 @@
+// Shared Dropbox folder containing the current .nxs release files for every
+// version 4 package (nexSys4, nexMap4, nexGui4, nexBash4). Keep this as the
+// single source of truth so the URL only needs to change in one place.
+export const DROPBOX_V4_DOWNLOADS_URL =
+  "https://www.dropbox.com/scl/fo/0zlqj0paorj6251q7eg64/AMnyOIzTb1WG7tBwVMSAORw/version%204?rlkey=zkf00szptvruwziqbji72q654&subfolder_nav_tracking=1&st=w6kpo6xz&dl=0";
